478b7e1ae9940477a7dff3c4475654b651c4d8b7
Gif002 <114986785+Gif002@users.noreply.github.com>
2022-12-10 02:39:24 +0000

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'0c8631f3-9058-4774-8aed-cc7b1068f942'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
